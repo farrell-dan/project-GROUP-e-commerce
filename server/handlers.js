3233d6f31560c24c84fcb4c9a7fb2e62b7383fa6
CharlesEricStLD <ce.stlouisdupuis+gitHub@gmail.com>
2023-11-16 12:49:23 -0500

    feat :require my new enpoint into handlers.js

--- server/handlers.js
+++ server/handlers.js
@@ -1,8 +1,16 @@
 const getUniqueCategories = require("./handlers/getUniqueCategories")
 const getItemsbyCategory = require ("./handlers/getItemsbyCategory")
+const buyerVerification = require("./handlers/buyerVerification")
+const buyItem = require("./handlers/buyItem")
+const getItemByID = require("./handlers/getItemByID");
+const addToCart = require("./handlers/addToCart")
 
 
 module.exports = {
     getUniqueCategories,
     getItemsbyCategory,
+    buyerVerification,
+    buyItem,
+    getItemByID,
+    addToCart
 }
