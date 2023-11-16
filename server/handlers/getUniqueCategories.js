5d73e2ee1161da551621bb0a26989284297eb0e7
Daniel Farrell <daniel@Daniels-MacBook-Pro.local>
2023-11-16 13:29:07 -0500

    getCategories & getItems

--- server/handlers/getUniqueCategories.js
+++ server/handlers/getUniqueCategories.js
@@ -1,20 +1,23 @@
 const { MongoClient } = require("mongodb");
+require("dotenv").config();
 const { MONGO_URI } = process.env;
+
 
 const getUniqueCategories = async (req, res) => {
     const client = new MongoClient(MONGO_URI);
   
     try {
       await client.connect();
-      const db = client.db("ECommerceWebsite ");
+      const db = client.db("ECommerceWebsite");
   
       const categories = await db.collection("Items").distinct("category");
   
       if (categories && categories.length > 0) {
-        res.status(200).json({ data: categories });
+        res.status(200).json({ status: 200, message: "Here are your categories", data: categories });
         console.log(categories)
       } else {
-        res.status(404).json({ error: "No categories found :(" });
+        res.status(404).json({ status: 404, message: "No categories found :(" });
+        console.log("No categories found :(")
       }
     } catch (error) {
       console.error(error);
