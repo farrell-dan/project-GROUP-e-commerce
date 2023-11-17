33ac5af568457839e5122c3d3db0413a16348dda
CharlesEricStLD <ce.stlouisdupuis+gitHub@gmail.com>
2023-11-17 12:38:14 -0500

    Fix : correct json error in buyerverification and index.js

--- server/handlers/buyerVerification.js
+++ server/handlers/buyerVerification.js
@@ -12,6 +12,8 @@ const BuyerVerification = async (request, response) => {
   const testVisaExpirationDate = new RegExp(/^\d{1,2}\/{1}\d{1,2}$/);
 
   const {firstName, lastName, creditCardNumber, expiration} = request.body;
+  
+  console.log(expiration);
 
   try {
 
@@ -21,13 +23,13 @@ const BuyerVerification = async (request, response) => {
     .json({status:404, message : `Please fill in all the required field, you forgot : (${firstName && firstName}), (${lastName && lastName}), ${creditCardNumber && creditCardNumber}, ${expiration && expiration}` })
   }
 
-  if (testVisaNumber.test(creditCardNumber)) {
+  if (testVisaNumber.test((Number(JSON.stringify(creditCardNumber))))) {
     return response
     .status(401)
     .json({status:401, message : `Please verify the card Number, card number can only contain numbers, you enter : ${creditCardNumber}` })
   }
   
-  if (testVisaExpirationDate.test(expiration)) {
+  if (testVisaExpirationDate.test(JSON.stringify(expiration))) {
     return response
     .status(401)
     .json({status:401, message : `Please verify the expiration number, you provided : ${expiration}`})
