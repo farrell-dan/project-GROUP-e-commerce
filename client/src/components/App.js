d95587fda228933819c40c6d9c5c2210ff1e1de0
CharlesEricStLD <ce.stlouisdupuis+gitHub@gmail.com>
2023-11-20 09:18:28 -0500

    Fix : remove unused Route of filtering page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,10 +8,7 @@ import OrderConfirmed from "./OrderConfirmed"
 import Header from "./Header"
 
 
-
-
 const App = () => {
-    
     return (
         <BrowserRouter>
             <GlobalStyle />
@@ -19,8 +16,7 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/category/:category" element={<ProductType />} />
-                <Route path="/product/:productId" element={<ProductPage />} />
-                <Route path="/product/sorting" element={<FilterByPriceLowToHigh/>} />                
+                <Route path="/product/:productId" element={<ProductPage />} />          
                 <Route path="/cart" element={<Cart />} />
                 {/* <Route path="/CheckoutPage" element={Checkout}/> */}
                 <Route path="/confirm/:orderId" element={<OrderConfirmed />} />
